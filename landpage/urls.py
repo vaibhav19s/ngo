2eb5378313bd43f974d41e1c3876bdb14bb73619
Vaibhav Sharma <vaibhavsharma1998@gmail.com>
2018-02-11 05:26:25 +0530

    signup for public complete

--- landpage/urls.py
+++ landpage/urls.py
@@ -20,20 +20,22 @@ urlpatterns = [
 
 
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login.html'}, name='logout'),
+    url(r'^logout/login.html/$', views.index, name='logout/login.html'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^accounts/profile/$', views.index, name='accounts/profile'),
 
 
     url(r'^(?P<event_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login_user/$', views.login_user, name='login_user'),
-    url(r'^logout_user/$', views.logout_user, name='logout_user'),
-    url(r'^events/(?P<filter_by>[a-zA_Z]+)/$', views.events, name='events'),
-    url(r'^create_ngo/$', views.create_ngo, name='create_ngo'),
-    url(r'^(?P<ngo_id>[0-9]+)/create_event/$', views.create_event, name='create_event'),
-    url(r'^(?P<ngo_id>[0-9]+)/delete_event/(?P<event_id>[0-9]+)/$', views.delete_event, name='delete_event'),
-    url(r'^(?P<ngo_id>[0-9]+)/favorite_ngo/$', views.favorite_ngo, name='favorite_ngo'),
-    url(r'^(?P<ngo_id>[0-9]+)/delete_ngo/$', views.delete_ngo, name='delete_ngo'),
+    #url(r'^register/$', views.register, name='register'),
+    #url(r'^login_user/$', views.login_user, name='login_user'),
+    #url(r'^logout_user/$', views.logout_user, name='logout_user'),
+    #url(r'^events/(?P<filter_by>[a-zA_Z]+)/$', views.events, name='events'),
+    #url(r'^create_ngo/$', views.create_ngo, name='create_ngo'),
+    #url(r'^(?P<ngo_id>[0-9]+)/create_event/$', views.create_event, name='create_event'),
+    #url(r'^(?P<ngo_id>[0-9]+)/delete_event/(?P<event_id>[0-9]+)/$', views.delete_event, name='delete_event'),
+    #url(r'^(?P<ngo_id>[0-9]+)/favorite_ngo/$', views.favorite_ngo, name='favorite_ngo'),
+    #url(r'^(?P<ngo_id>[0-9]+)/delete_ngo/$', views.delete_ngo, name='delete_ngo'),
 
 ]
